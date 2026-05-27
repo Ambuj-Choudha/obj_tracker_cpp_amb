9c0c4533e57163da2d7b1eaa2484784e2f5dde9e
Ambuj Choudha <ambuj.choudha@rwth-aachen.de>
2026-05-28 00:53:13 +0200

    added: an abstract base class for common methods

--- include/camera.hpp
+++ include/camera.hpp
@@ -9,14 +9,37 @@ namespace Defaults {
     inline constexpr int FrameDefaultHeight = 720;
 }
 
-
+// Pure interface
 class IInputSource {
     public:
         virtual ~IInputSource() = default;
         virtual std::optional<cv::Mat> getNextFrame() = 0;
+        virtual double getFps() const = 0;
 };
 
-class WebcamCamera : public IInputSource{
+// Abstract base class
+class VideoCaptureBase : public IInputSource {
+    public:
+        double getFps() const override { return current_fps_; }
+
+    protected:
+        cv::VideoCapture cap;
+
+        void updateFps() {
+            int64_t current_tick = cv::getTickCount();
+            double time_delta = static_cast<double>(current_tick - last_frame_tick_) / cv::getTickFrequency();
+            if (time_delta > 0.0) {
+                current_fps_ = 1.0 / time_delta;
+            }
+            last_frame_tick_ = current_tick;
+        }
+
+private:
+    double current_fps_{0.0};
+    int64_t last_frame_tick_{cv::getTickCount()};
+};
+
+class WebcamCamera : public VideoCaptureBase {
     public:
         WebcamCamera(int deviceID = 0, int apiID = cv::CAP_ANY);
         WebcamCamera(const WebcamCamera&) = delete;  // copy constructor
@@ -29,10 +52,9 @@ class WebcamCamera : public IInputSource{
     private:
         int deviceID;
         int apiID;
-        cv::VideoCapture cap;
 };
 
-class VideoFile : public IInputSource{
+class VideoFile : public VideoCaptureBase {
     public:
         VideoFile(const std::string& source_file, int apiID = cv::CAP_ANY);
         VideoFile(const VideoFile&) = delete;
@@ -41,8 +63,8 @@ class VideoFile : public IInputSource{
         ~VideoFile() override = default;
 
         std::optional<cv::Mat> getNextFrame() override;
+        
     private:
         std::string source_file;
         int apiID;
-        cv::VideoCapture cap;
 };
